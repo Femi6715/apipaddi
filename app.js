09ec8240db5d983cd8b15cb3e6f98e38d092a843
femi <femialao6920@gmail.com>
2025-05-17 19:23:06 -0400

    Switch from MongoDB to MySQL: Update database configuration and queries

--- app.js
+++ app.js
@@ -3,29 +3,35 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
-const mongoose = require('mongoose');
+const mysql = require('mysql2');
 const config = require('./config/database');
 const cron = require('node-cron');
 const moment = require('moment');
 const helmet = require('helmet');
 
 require('dotenv').config();
-
 require('./config/passport');
-// const { ObjectID } = require('mongodb');
-const MongoClient = require('mongodb').MongoClient;
-//connecting to database
-mongoose.connect(config.database, { useNewUrlParser: true });
-const { ObjectID } = require('mongodb');
 
-//test out connection and log status to console.
-mongoose.connection.on('connected', () => {
-  console.log('connected to the database ' + config.database);
+// Create MySQL connection pool
+const pool = mysql.createPool({
+    host: config.database.host,
+    user: config.database.user,
+    password: config.database.password,
+    database: config.database.database,
+    port: config.database.port,
+    waitForConnections: true,
+    connectionLimit: config.database.pool.max,
+    queueLimit: 0
 });
 
-//Check for database connection error
-mongoose.connection.on('error', err => {
-  console.log('Database Error' + err);
+// Test database connection
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.log('Database Error:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+    connection.release();
 });
 
 const app = express();
@@ -39,24 +45,15 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const users = require('./routes/users');
-
 const games = require('./routes/games');
-
 const category = require('./routes/games-categories');
-
 const transactions = require('./routes/transactions');
-
 const winners = require('./routes/winning-tickets');
-
 const admin = require('./routes/admins');
-
 const transfer = require('./routes/transfer_recipients');
-
 const tickets = require('./routes/tickets');
 
 const port = process.env.PORT || 8080;
-
-// const port = 3000;
 
 app.use(helmet());
 
@@ -71,28 +68,21 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/users', users);
-
 app.use('/games', games);
-
 app.use('/categories', category);
-
 app.use('/transactions', transactions);
-
 app.use('/winners', winners);
-
 app.use('/admin', admin);
-
 app.use('/tickets', tickets);
-
 app.use('/transfer', transfer);
 
 app.get('/', (req, res) => {
-  res.send('Welcome to PADI LOTTO api');
-  console.log('there is a hit');
+    res.send('Welcome to PADI LOTTO api');
+    console.log('there is a hit');
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const currentDay = new Date(new Date().getTime());
@@ -146,120 +136,89 @@ detailsDetector('6', res => {
 });
 
 let shuffleMachine = our_stake_amt => {
-  MongoClient.connect(
-    config.database,
-    { useNewUrlParser: true },
-    (err, client) => {
-      if (err) {
-        return console.log('Unable to connect to mongodb database');
-      } else {
-        const db = client.db('simplelotto');
-        const shuffleAlert = todaysDate;
-        const ticket_status = 'pending';
-        const stake_amt = our_stake_amt;
-        db.collection('games')
-          .countDocuments({
-            draw_date: shuffleAlert,
-            ticket_status: ticket_status,
-            stake_amt: stake_amt
-          })
-          .then(count => {
-            this.totalCount = count - 1;
-            db.collection('games')
-              .find(
-                {
-                  draw_date: shuffleAlert,
-                  ticket_status: 'pending',
-                  stake_amt: stake_amt
-                },
-                { skip: this.totalCount }
-              )
-              .toArray()
-              .then(docs => {
-                const obj = Object.keys(docs);
-                item = obj[Math.floor(Math.random() * obj.length)];
-
-                db.collection('games')
-                  .findOneAndUpdate(
-                    {
-                      _id: docs[item]._id
-                    },
-                    {
-                      $set: {
-                        ticket_status: 'won'
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.log('Database Error:', err);
+      return;
+    }
+    const shuffleAlert = todaysDate;
+    const ticket_status = 'pending';
+    const stake_amt = our_stake_amt;
+    connection.query(
+      'SELECT * FROM games WHERE draw_date = ? AND ticket_status = ? AND stake_amt = ?',
+      [shuffleAlert, ticket_status, stake_amt],
+      (err, results) => {
+        if (err) {
+          console.log('Database Error:', err);
+          connection.release();
+          return;
+        }
+        if (results.length > 0) {
+          const obj = Object.keys(results);
+          const item = obj[Math.floor(Math.random() * obj.length)];
+          connection.query(
+            'UPDATE games SET ticket_status = ? WHERE _id = ?',
+            ['won', results[item]._id],
+            (err, result) => {
+              if (err) {
+                console.log('Database Error:', err);
+                connection.release();
+                return;
+              }
+              connection.query(
+                'SELECT main_balance FROM users WHERE _id = ?',
+                [results[item].user_id],
+                (err, winner_info) => {
+                  if (err) {
+                    console.log('Database Error:', err);
+                    connection.release();
+                    return;
+                  }
+                  const winnerBalance = winner_info[0].main_balance;
+                  const winnerId = results[item].user_id;
+                  let amount_won = parseInt(
+                    results[item].potential_winning,
+                    10
+                  );
+                  let new_balance = winnerBalance + amount_won;
+                  connection.query(
+                    'UPDATE users SET main_balance = ? WHERE _id = ?',
+                    [new_balance, winnerId],
+                    (err, result) => {
+                      if (err) {
+                        console.log('Database Error:', err);
+                        connection.release();
+                        return;
                       }
-                    },
-                    { new: true }
-                  )
-                  .then(
-                    result => {
-                      db.collection('users').findOne(
-                        { _id: ObjectID(docs[item].user_id) },
-                        function(err, winner_info) {
-                          if (err) throw err;
-                          const winnerBalance = winner_info.main_balance;
-                          const winnerId = winner_info._id;
-                          let amount_won = parseInt(
-                            docs[item].potential_winning,
-                            10
-                          );
-                          let new_balance = winnerBalance + amount_won;
-
-                          db.collection('users')
-                            .findOneAndUpdate(
-                              {
-                                _id: winnerId
-                              },
-                              {
-                                $set: {
-                                  main_balance: new_balance
-                                }
-                              },
-                              { new: true }
-                            )
-                            .then(
-                              result => {},
-                              status_err => {
-                                console.log(
-                                  `TICKET STATUS UPDATE FAILED: ${status_err}`
-                                );
-                              }
-                            );
-                        },
-                        status_err => {
-                          console.log(
-                            `TICKET STATUS UPDATE FAILED: ${status_err}`
-                          );
-                        }
-                      );
-                    },
-                    err => {
-                      console.log(`UNABLE TO FETCH GAME ${err}`);
+                      console.log('Winner updated successfully');
+                      connection.release();
                     }
                   );
-              })
-              .catch(err =>
-                console.log('ERROR: could not fetch at this time' + err)
+                }
               );
-          })
-          .catch(error => console.log('error - maybe there are no games'));
+            }
+          );
+        } else {
+          console.log('No games found');
+          connection.release();
+        }
       }
-    }
-  );
+    );
+  });
 };
 
 let num_of_winners = (num, stake_amt) => {
-  for (i = 0; i < num; i++) {
-    if (shuffleDates !== undefined) {
-      for (let eachShuffleDate of shuffleDates) {
-        if (todaysDate === eachShuffleDate) {
-          cron.schedule(`${05 + i} 10 18 * * *`, () => {
-            shuffleMachine(stake_amt);
-          });
+    for (i = 0; i < num; i++) {
+        if (shuffleDates !== undefined) {
+            for (let eachShuffleDate of shuffleDates) {
+                if (todaysDate === eachShuffleDate) {
+                    cron.schedule(`${5 + i} 10 18 * * *`, () => {
+                        shuffleMachine(stake_amt);
+                    });
+                }
+            }
         }
-      }
     }
-  }
 };
 
 num_of_winners(0, 25);
@@ -270,60 +229,21 @@ if (shuffleDates !== undefined) {
   for (let eachShuffleDate of shuffleDates) {
     if (todaysDate === eachShuffleDate) {
       cron.schedule('50 10 18 * * *', function() {
-        MongoClient.connect(
-          config.database,
-          { useNewUrlParser: true },
-          (err, client) => {
+        pool.query(
+          'UPDATE games SET ticket_status = ? WHERE ticket_status = ? AND draw_date = ?',
+          ['lost', 'pending', todaysDate],
+          (err, result) => {
             if (err) {
-              return console.log('Unable to connect to mongodb database');
-            } else {
-              const db = client.db('simplelotto');
-              const shuffleAlert = todaysDate;
-              const ticket_status = 'pending';
-              const shuffleInfo = {
-                draw_date: shuffleAlert,
-                ticket_status: ticket_status
-              };
-              db.collection('games')
-                .updateMany(
-                  {
-                    ticket_status: ticket_status,
-                    draw_date: shuffleAlert
-                  },
-                  {
-                    $set: {
-                      ticket_status: 'lost'
-                    }
-                  },
-                  { new: true }
-                )
-                .then(
-                  result => {},
-                  err => {
-                    console.log(`UNABLE TO UPDATE GAME ${err}`);
-                  }
-                );
+              console.log('Error updating games:', err);
+              return;
             }
+            console.log('Games updated successfully');
           }
         );
       });
     }
   }
 }
-
-
-
-
-// // Enable CORS for all routes or specify the allowed origin
-// app.use(cors({
-//   origin: 'http://localhost:4200'  // Adjust as needed or use '*' for all origins
-// }));
-
-// // // Your other middleware and routes
-// // app.use('/users', require('./routes/users'));
-
-// // const PORT = process.env.PORT || 3000;
-
 
 app.listen(port, () => {
   console.log('Server running on port ' + port);
